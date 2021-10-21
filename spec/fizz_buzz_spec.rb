a83e7760d746d5580a50c403242b44e72209a019
DefyCab <love.bellinder@gmail.com>
2021-10-21 10:51:39 +0200

    first test returning number

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../src/fizz_buzz'
+
 describe FizzBuzz do
 
     subject {FizzBuzz.new}
@@ -7,16 +9,16 @@ describe FizzBuzz do
     
     end
 
-    it "is expected to return 'fizz' if the number is divisible by 3" do
+    xit "is expected to return 'fizz' if the number is divisible by 3" do
         expect{subject.check(3)}.to eq "fizz"
     end
 
-    it "is expected to return 'buzz' if the number is divisible by 5" do
+    xit "is expected to return 'buzz' if the number is divisible by 5" do
         expect{subject.check(5)}.to eq "buzz"
 
     end
 
-    it "is expected to return 'fizz-buzz' if the number is divisible by 15" do
+    xit "is expected to return 'fizz-buzz' if the number is divisible by 15" do
         expect{subject.check(15)}.to eq "fizz-buzz"
 
     end
